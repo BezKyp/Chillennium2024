38b951954566e6334bb991e4d5c6d9b624e9905c
larasoberanis <40220955+larasoberanis@users.noreply.github.com>
2024-03-02 13:22:56 -0600

    finally fixed anim loop and screen size

--- Assets/Scripts/General/ScreenSize.cs
+++ Assets/Scripts/General/ScreenSize.cs
@@ -7,13 +7,29 @@ public class ScreenSize : MonoBehaviour
 {
 
     public Camera cam;
+
+    private int sch;
     // Start is called before the first frame update
     void Start()
     {
         //cam.rect = new Rect(0.25f, 0f, 0.5f, 1.0f);
+        sch = Screen.height;
         float scale = ((float) Screen.height) / ((float) Screen.width);
         float off = (1 - scale) / 2;
         cam.rect = new Rect(off, 0f, scale, 1.0f);
+
+    }
+
+    private void Update()
+    {
+        if (sch != Screen.height)
+        {
+            sch = Screen.height;
+            float scale = ((float)Screen.height) / ((float)Screen.width);
+            float off = (1 - scale) / 2;
+            cam.rect = new Rect(off, 0f, scale, 1.0f);
+        }
+
 
     }
 
