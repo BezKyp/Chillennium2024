498a4fa37034b0e82353715b5ffd717ebb77f25f
larasoberanis <40220955+larasoberanis@users.noreply.github.com>
2024-03-02 13:09:10 -0600

    camera and fixing anims

--- Assets/Scripts/General/PlayerScripts/PlayerMovement.cs
+++ Assets/Scripts/General/PlayerScripts/PlayerMovement.cs
@@ -6,13 +6,16 @@ public class PlayerMovement : MonoBehaviour
 {
     public float moveSpeed;
     public Rigidbody2D Rb;
+    public Animator anim;
 
     private Vector2 moveDirection;
+    private Vector2 lastMoveDirection;
 
     // Update is called once per frame
     void Update()
     {
         processInputs();
+        Animate();
     }
 
     private void FixedUpdate()
@@ -25,13 +28,28 @@ public class PlayerMovement : MonoBehaviour
         float moveX = Input.GetAxisRaw("Horizontal");
         float moveY = Input.GetAxisRaw("Vertical");
 
+        if ((moveX == 0 && moveY == 0) && moveDirection.x != 0 || moveDirection.y != 0)
+        {
+            lastMoveDirection = moveDirection;
+        }
+
         moveDirection = new Vector2(moveX, moveY).normalized;
+
 
     }
 
     private void Move()
     {
         Rb.velocity = new Vector2(moveDirection.x * moveSpeed, moveDirection.y * moveSpeed);
+    }
+
+    void Animate()
+    {
+        anim.SetFloat("AnimMoveX", moveDirection.x);
+        anim.SetFloat("AnimMoveY", moveDirection.y);
+        anim.SetFloat("AnimMoveMagnitude", moveDirection.magnitude);
+        anim.SetFloat("AnimLastMoveX", lastMoveDirection.x);
+        anim.SetFloat("AnimLastMoveY", lastMoveDirection.y);
     }
 
 }
--- Assets/Scripts/General/ScreenSize.cs
+++ Assets/Scripts/General/ScreenSize.cs
@@ -10,7 +10,11 @@ public class ScreenSize : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        cam.rect = new Rect(0.25f, 0f, 0.5f, 1.0f);
+        //cam.rect = new Rect(0.25f, 0f, 0.5f, 1.0f);
+        float scale = ((float) Screen.height) / ((float) Screen.width);
+        float off = (1 - scale) / 2;
+        cam.rect = new Rect(off, 0f, scale, 1.0f);
+
     }
 
 }
